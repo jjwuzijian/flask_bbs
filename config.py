4516d8d4fc3e174d694b989f3c4b042480035706
wuzijian <2662147461@qq.com>
2018-09-29 09:30:02 +0800

    captcha

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 SECRET_KEY = os.urandom(24)
 DEBUG = True
 
-# DB_URI = 'mysql+pymysql://root:1234@localhost:3306/bbs?charset=utf8'
-DB_URI = 'mysql+pymysql://root:123456@localhost:3306/bbs?charset=utf8'
+DB_URI = 'mysql+pymysql://root:1234@localhost:3306/bbs?charset=utf8'
+# DB_URI = 'mysql+pymysql://root:123456@localhost:3306/bbs?charset=utf8'
 SQLALCHEMY_DATABASE_URI = DB_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
